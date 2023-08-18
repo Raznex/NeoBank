06beeab5e5cc11d6fa19f0997dc07f478ac33a0d
Raznex <Dudya15@yandex.ru>
2023-08-18 17:28:41 +0300

    Создал реакт приложение

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './common/assets/normalize/_reset.scss';
+import './common/assets/fonts/_fonts.scss';
+import App from './Modules/Module-3/components/App/App';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
