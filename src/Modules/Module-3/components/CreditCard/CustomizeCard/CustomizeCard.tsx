8d89237acede0764fb6bf1b92108ef8fb964e49b
Raznex <Dudya15@yandex.ru>
2023-08-22 18:15:53 +0300

    Сделал форму, пока без валидации

--- src/Modules/Module-3/components/CreditCard/CustomizeCard/CustomizeCard.tsx
+++ src/Modules/Module-3/components/CreditCard/CustomizeCard/CustomizeCard.tsx
@@ -1,7 +1,22 @@
-import React from 'react';
 import './_CustomizeCard.scss'
+import {
+  CheckFill,
+  CloseRoundFill,
+  ExpandDown
+} from "../../../../../common/assets/icon/moduleIcon/index";
+import React, {useState} from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const CustomizeCard = () => {
+  const [startDate, setStartDate] = useState(null);
+  const [isInputFocused, setIsInputFocused] = useState(false);
+
+  const handleDateChange = (date:any) => {
+    setStartDate(date);
+    setIsInputFocused(false); // Закрываем календарь при выборе даты
+  };
+
   return (
     <form className="customize-card">
       <div className="customize-card__container-upper">
@@ -25,22 +40,167 @@ const CustomizeCard = () => {
         <div className="customize-card__form">
           <article className="customize-card__inputs">
             <div className="customize-card__input-box">
-              <label htmlFor="email-field" className="customize-card__lable">E-mail <span className="customize-card__lable-span">*</span></label>
+              <label htmlFor="lastName-field" className="customize-card__lable">Your last name <span
+                className="customize-card__lable-span">*</span></label>
               <input
                 type="text"
+                id="lastName-field"
+                className="customize-card__input"
+                minLength={2}
+                maxLength={40}
+                required
+                name="lastName"
+                placeholder="For Example Doe"
+                // onChange={handleControl}
+              />
+              <CheckFill className="customize-card__icon"/>
+              <CloseRoundFill className="customize-card__icon"/>
+            </div>
+            <span className="email-field-error customize-card__span"></span>
+          </article>
+          <article className="customize-card__inputs">
+            <div className="customize-card__input-box">
+              <label htmlFor="firstName-field" className="customize-card__lable">Your first name <span
+                className="customize-card__lable-span">*</span></label>
+              <input
+                type="text"
+                id="firstName-field"
+                className="customize-card__input"
+                minLength={2}
+                maxLength={40}
+                required
+                name="firstName"
+                placeholder="For Example Jhon"
+                // onChange={handleControl}
+              />
+              <CheckFill className="customize-card__icon"/>
+              <CloseRoundFill className="customize-card__icon"/>
+            </div>
+            <span className="email-field-error customize-card__span"></span>
+          </article>
+          <article className="customize-card__inputs">
+            <div className="customize-card__input-box">
+              <label htmlFor="middleName-field" className="customize-card__lable">Your patronymic <span
+                className="customize-card__lable-span"></span></label>
+              <input
+                type="text"
+                id="middleName-field"
+                className="customize-card__input"
+                minLength={2}
+                maxLength={40}
+                name="middleName"
+                placeholder="For Example Victorovich"
+                // onChange={handleControl}
+              />
+              <CheckFill className="customize-card__icon"/>
+              <CloseRoundFill className="customize-card__icon"/>
+            </div>
+            <span className="email-field-error customize-card__span"></span>
+          </article>
+          <article className="customize-card__inputs">
+            <div className="customize-card__input-box">
+              <label htmlFor="term-field" className="customize-card__lable">Select term <span
+                className="customize-card__lable-span">*</span></label>
+              <select
+                id="term-field"
+                className="customize-card__input customize-card__input_select"
+                required
+                name="term"
+                // onChange={handleControl}
+              >
+                <option className="customize-card__options">6 month</option>
+                <option className="customize-card__options">12 month</option>
+                <option className="customize-card__options">18 month</option>
+                <option className="customize-card__options">24 month</option>
+              </select>
+              <ExpandDown className="customize-card__expand"/>
+            </div>
+            <span className="email-field-error customize-card__span"></span>
+          </article>
+          <article className="customize-card__inputs">
+            <div className="customize-card__input-box">
+              <label htmlFor="middleName-field" className="customize-card__lable">Your email <span
+                className="customize-card__lable-span">*</span></label>
+              <input
+                type="email"
                 id="email-field"
                 className="customize-card__input"
                 minLength={2}
                 maxLength={40}
                 required
                 name="email"
-                placeholder="For Example Doe"
-                // value={isEditing ? formValue.email : currentUser.email}
+                placeholder="test@gmail.com"
                 // onChange={handleControl}
               />
+              <CheckFill className="customize-card__icon"/>
+              <CloseRoundFill className="customize-card__icon"/>
             </div>
             <span className="email-field-error customize-card__span"></span>
           </article>
+          <article className="customize-card__inputs">
+            <div className="customize-card__input-box">
+              <label htmlFor="middleName-field" className="customize-card__lable">
+                Your date of birth <span className="customize-card__lable-span">*</span>
+              </label>
+              <div className={`customize-card__input ${isInputFocused ? 'customize-card__input_focus' : ''}`}>
+                <DatePicker
+                  selected={startDate}
+                  onChange={handleDateChange}
+                  onFocus={() => setIsInputFocused(true)} // Обработчик фокуса на поле ввода
+                  onBlur={() => setIsInputFocused(false)} // Обработчик потери фокуса
+                  placeholderText={isInputFocused ? '' : 'Select Date and Time'}
+                  dateFormat="dd-MM-yyyy"
+                  open={isInputFocused} // Показываем календарь, если поле в фокусе
+                />
+              </div>
+              <CheckFill className="customize-card__icon" />
+              <CloseRoundFill className="customize-card__icon" />
+            </div>
+            <span className="email-field-error customize-card__span"></span>
+          </article>
+          <article className="customize-card__inputs">
+            <div className="customize-card__input-box">
+              <label htmlFor="middleName-field" className="customize-card__lable">Your passport series <span
+                className="customize-card__lable-span">*</span></label>
+              <input
+                type="text"
+                id="passSeries-field"
+                className="customize-card__input"
+                minLength={4}
+                maxLength={4}
+                required
+                name="passSeries"
+                placeholder="0000"
+                // onChange={handleControl}
+              />
+              <CheckFill className="customize-card__icon"/>
+              <CloseRoundFill className="customize-card__icon"/>
+            </div>
+            <span className="email-field-error customize-card__span"></span>
+          </article>
+          <article className="customize-card__inputs">
+            <div className="customize-card__input-box">
+              <label htmlFor="middleName-field" className="customize-card__lable">Your passport number <span
+                className="customize-card__lable-span">*</span></label>
+              <input
+                type="text"
+                id="passNumber-field"
+                className="customize-card__input"
+                minLength={6}
+                maxLength={6}
+                required
+                name="passNumber"
+                placeholder="000000"
+                // onChange={handleControl}
+              />
+              <CheckFill className="customize-card__icon"/>
+              <CloseRoundFill className="customize-card__icon"/>
+            </div>
+            <span className="email-field-error customize-card__span"></span>
+          </article>
+        </div>
+        <div className="customize-card__button-container">
+        <button className="customize-card__button">Continue</button>
         </div>
       </div>
     </form>
---
+++ src/common/assets/icon/moduleIcon/CloseRoundFill.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+const SvgCloseRoundFill = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="none" viewBox="0 0 24 24" {...props}>
+    <path
+      fill="#FF5631"
+      fillRule="evenodd"
+      d="M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0ZM7.293 16.707a1 1 0 0 1 0-1.414L10.586 12 7.293 8.707a1 1 0 0 1 1.414-1.414L12 10.586l3.293-3.293a1 1 0 1 1 1.414 1.414L13.414 12l3.293 3.293a1 1 0 0 1-1.414 1.414L12 13.414l-3.293 3.293a1 1 0 0 1-1.414 0Z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+export default SvgCloseRoundFill;
